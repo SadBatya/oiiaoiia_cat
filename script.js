fde01cd12e674fef25f65a2de2e6dc8ed29815d1
sadbatya <sadbatya@mail.ru>
2025-07-14 22:10:58 +0200

    feat: overlay

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const player = document.querySelector(".player");
 const button = document.querySelector(".button_player");
+const overlay = document.querySelector(".overlay");
 
 button.addEventListener("click", () => {
   if (player.paused) {
     player.play();
+    overlay.style.display = "none";
     button.style.display = "none";
   } else {
     player.pause();
